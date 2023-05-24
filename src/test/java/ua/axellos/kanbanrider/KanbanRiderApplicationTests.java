c564393a2f4e4ae1abb47f421e7a05f39e25fa6e
Andrii Pidhornyi <a.pidhornyi.dev@gmail.com>
2023-05-24 18:33:50 +0300

    initialize project structure

---
+++ src/test/java/ua/axellos/kanbanrider/KanbanRiderApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.axellos.kanbanrider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanRiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
