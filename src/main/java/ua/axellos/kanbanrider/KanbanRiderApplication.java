c4bd17024bbfe1d1b10c41b52c68cdcc3c0d7091
Andrii Pidhornyi <a.pidhornyi.dev@gmail.com>
2023-05-24 18:33:50 +0300

    chore: initialize project structure

---
+++ src/main/java/ua/axellos/kanbanrider/KanbanRiderApplication.java
@@ -0,0 +1,13 @@
+package ua.axellos.kanbanrider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanRiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanRiderApplication.class, args);
+    }
+
+}
