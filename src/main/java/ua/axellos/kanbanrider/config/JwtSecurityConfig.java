546316bb93625404898a555c2bf9452c8eda69c8
Andrii Pidhornyi <a.pidhornyi.dev@gmail.com>
2023-08-15 22:52:10 +0300

    feat(security): add cors support

--- src/main/java/ua/axellos/kanbanrider/config/JwtSecurityConfig.java
+++ src/main/java/ua/axellos/kanbanrider/config/JwtSecurityConfig.java
@@ -6,16 +6,31 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class JwtSecurityConfig {
 
-  @Bean
-  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .cors();
 
-    return http.build();
-  }
+        return http.build();
+    }
+
+    @Bean
+    WebMvcConfigurer WebMvcConfigurer() {
+
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
